15412dbbb4e88b7afdcd3df43ecb6d8d6c81d45f
James Bunker <james@jamesbunker.com>
2017-05-16 16:36:56 +0100

    modified flow, added ffmpeg classes

--- app.php
+++ app.php
@@ -3,23 +3,31 @@ require('config.php');
 require('classes/database.php');
 require('aws/vendor/autoload.php');
 require('vision/vendor/autoload.php');
+require('ffmpeg/vendor/autoload.php');
 
-# 1 - get the token name
+# 1 - get the token name, creation type (random|manual) + cdn linked images - if manual, 5 stored in db, if random, 10 stored and picked from db at random
 # 2 - create a database session
 # 3 - create s3 project folder inside bucket
-# 3 - get images from db {
+# 4 - get images from instagram and store in s3 and db against token
+# 5 - get images from db {
 
 // SELECT s.* FROM instanceSlides s JOIN instances I ON s.instanceID = i.id ORDER BY RAND()
 
-	# 4 - get images from instagram and store in s3
-	# 5 - send images to cloud vision OR aws rekognition - store output in db
-	# 6 - generate lyrics from tags via polly, store in s3
 
+	# 6 - send images to cloud vision OR aws rekognition - store output in db
+	# 	this is where we decide to reject an image. 
+	# 7 - generate lyrics from tags via polly, store in s3
+
+	# SELECT s.* FROM instanceSlides s JOIN instances I ON s.instanceID = i.id  WHERE status = 'accepted'
+	# if (slides < 4) {
+		# random? pick another couple of images
+		# manual - bail out message
+	#}
 
 # }
-# 7 - time lyrics
+# 8 - time lyrics 
 
-# 8 - merge audio in ffmpeg,
-# 9 - merge images to audio track
-# 10 - store video in s3
-# 11 - update db + kill this session
+# 9 - merge audio in ffmpeg,
+# 10 - merge images to audio track
+# 11 - store video in s3
+# 12 - update db + kill this session
