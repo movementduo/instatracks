108e7640b13791204239812eb4d611e82e0cbba5
James Bunker <james@jamesbunker.com>
2017-05-16 17:01:57 +0100

    instantiate db, set up sessions

--- app.php
+++ app.php
@@ -5,8 +5,37 @@ require('aws/vendor/autoload.php');
 require('vision/vendor/autoload.php');
 require('ffmpeg/vendor/autoload.php');
 
+session_start();
+
 # 1 - get the token name, creation type (random|manual) + cdn linked images - if manual, 5 stored in db, if random, 10 stored and picked from db at random
+
 # 2 - create a database session
+$db = new Database($cfg);
+
+
+
+/*
+
+
+use Aws\S3\S3Client;
+
+// Instantiate an Amazon S3 client.
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => 'eu-west-1'
+]);
+
+try {
+    $s3->putObject([
+        'Bucket' => 'instatracks-v3',
+        'Key'    => 'instagram',
+        'Body'   => fopen('/home/ubuntu/basquiat.jpg', 'r'),
+        'ACL'    => 'public-read',
+    ]);
+} catch (Aws\S3\Exception\S3Exception $e) {
+    echo "There was an error uploading the file.\n";
+}*/
+
 # 3 - create s3 project folder inside bucket
 # 4 - get images from instagram and store in s3 and db against token
 # 5 - get images from db {
@@ -29,5 +58,30 @@ require('ffmpeg/vendor/autoload.php');
 
 # 9 - merge audio in ffmpeg,
 # 10 - merge images to audio track
+/*
+STEPS:
+1) concat 10 speech tracks into one file
+
+         ffmpeg -i aud001.wav -i aud002.wav -i aud003.wav -i aud004.wav -i aud005.wav -i aud006.wav -i aud007.wav -i aud008.wav -i aud009.wav -i aud010.wav -f lavfi -i anullsrc -filter_complex \
+"[10]atrim=duration=0.7[g1];[10]atrim=duration=0.7[g2];[10]atrim=duration=0.7[g3];[10]atrim=duration=0.7[g4];[10]atrim=duration=0.7[g5];[10]atrim=duration=0.7[g6];[10]atrim=duration=0.7[g7];[10]atrim=duration=0.7[g8];[10]atrim=duration=0.7[g9];
+ [0][g1][1][g2][2][g3][3][g4][4][g5][5][g6][6][g7][7][g8][8][g9][9]concat=n=19:v=0:a=1"  out.wav
+
+
+2) merge speech track with backing track
+
+ffmpeg -i backing.mp3 -i out.wav -filter_complex amerge -ac 2 -c:a libmp3lame -q:a 4 finished.mp3
+
+3) merge video with final audio track
+
+ffmpeg -i video.mp4 -i finished.mp3 \
+-c:v copy -c:a aac -strict experimental \
+-map 0:v:0 -map 1:a:0 fin.mp4
+
+
+overlay image ("overlay=20:20 top position in pixels", "between(t,0,25) appears from 0-25 seconds")
+ffmpeg -i fin.mp4 -i fanta_logo2.png -filter_complex "[0:v][1:v] overlay=20:20:enable='between(t,0,25)'" fin-overlay2.mp4
+*/
 # 11 - store video in s3
 # 12 - update db + kill this session
+
+session_destroy();
