64ed568bde1194b1d18b6867470bfb8bec59813c
Jason Luu <jasoncluu@googlemail.com>
2017-07-14 16:24:29 +0100

    todo functions'

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+
+function checkLabels($data, $i){
+
+}
+
+function checkLogos($data, $i){
+
+}
+
+function checkLandmarks($data, $i){
+
+}
+
+function checkFaces($data, $i){
+
+}
+
+function checkText($data, $i){
+
+}
+
+//Things to turn into functions:
+
+// Adding lyrics to image object
+// $myPics[] = (object) ["type"=>'noun', "text"=>$allLabels, "id"=>$i->id, "url"=>$i->url, "likes"=>$i->likes, "tagged"=>$i->taggedUsers, "lyrics"=>$lyric];
+
+// The rhyme scheme
