d2f82665ebdfdca7d7f0963917d69b3e2e51b6f3
James Bunker <james@jamesbunker.com>
2017-07-21 15:18:53 +0100

    mods

--- components/instagram_page.php
+++ components/instagram_page.php
@@ -23,14 +23,53 @@
   // receive OAuth token object
 		  $data = $instagram->getOAuthToken($_SESSION['oauthToken']);
 		  $username = $username = $data->user->username;
-echo $username.'<br /><br />';
 		  // store user access token
 		  $instagram->setAccessToken($data);
 
 		  // now you have access to all authenticated user methods
 		  $result = $instagram->getUserMedia();
+?>
 
-	die('<pre>'.var_export($result,true));
+        <h1><?php echo $data->user->username ?></h1>
+        <ul>
+        <?php
+          // display all user likes
+          foreach ($result->data as $media) {
+            $content = "<li>";
+
+            // output media
+            if ($media->type === 'video') {
+              // video
+              $poster = $media->images->low_resolution->url;
+              $source = $media->videos->standard_resolution->url;
+              $content .= "<video class=\"media video-js vjs-default-skin\" width=\"250\" height=\"250\" poster=\"{$poster}\"
+                           data-setup='{\"controls\":true, \"preload\": \"auto\"}'>
+                             <source src=\"{$source}\" type=\"video/mp4\" />
+                           </video>";
+            } else {
+              // image
+              $image = $media->images->low_resolution->url;
+              $content .= "<img class=\"media\" src=\"{$image}\"/>";
+            }
+
+            // create meta section
+            $avatar = $media->user->profile_picture;
+            $username = $media->user->username;
+            $comment = $media->caption->text;
+            $content .= "<div class=\"content\">
+                           <div class=\"avatar\" style=\"background-image: url({$avatar})\"></div>
+                           <p>{$username}</p>
+                           <div class=\"comment\">{$comment}</div>
+                         </div>";
+
+            // output media
+            echo $content . "</li>";
+          }
+        ?>
+        </ul>
+
+<?php
+
 } else {
 
   // check whether an error occurred
