d3ab1bee21f8cc46cc7b8ac764e4ca947653b3a8
James Bunker <james@jamesbunker.com>
2017-08-03 14:34:21 +0100

    ffmpeg tests

--- components/v_page.php
+++ components/v_page.php
@@ -12,6 +12,7 @@
 			if(array_key_exists(1,$this->args)) {
 				$this->video = $this->db->executeSql("SELECT * FROM instances WHERE instanceId = :x1 AND status = 'complete' LIMIT 1",[$this->args[1]])->fetchAssoc()[0];
 			}
+			die('<pre>'.var_export($this->video,true));
 			
 			if(!$this->video) {
 				die('error');
