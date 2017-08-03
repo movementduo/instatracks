5a7b1351cd3d657153a7401495935d4beceb1394
James Bunker <james@jamesbunker.com>
2017-08-03 10:58:36 +0100

    ffmpeg tests

--- templates/_layout.php
+++ templates/_layout.php
@@ -6,12 +6,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="robots" content="index">
 	<title><?php echo $title; ?></title>
-	<link href="assets/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
-	<link href="assets/css/style.css" type="text/css" rel="stylesheet" />
+	<link href="/assets/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+	<link href="/assets/css/style.css" type="text/css" rel="stylesheet" />
+	<script src="/assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
+	<script src="/assets/js/script.js" type="text/javascript"></script>
 </head>
 	<body>
 		<?php echo $body; ?>
 	</body>
-	<script src="assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
-	<script src="assets/js/script.js" type="text/javascript"></script>
 </html>
