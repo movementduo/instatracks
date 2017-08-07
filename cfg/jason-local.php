31deb413338811d0216f8b69bce59562c6d5f229
Jason Luu <jasoncluu@googlemail.com>
2017-08-07 13:31:38 +0100

    jason local config file

---
+++ cfg/jason-local.php
@@ -0,0 +1,12 @@
+<?php
+
+# config values
+
+define("DBHOST","localhost");
+define("DBUSER","root");
+define("DBPASS","jason_root");
+define("DBFILE","instatracks");
+
+define("APP_ROOT","/var/www/instatrackslamp/");
+define("WEB_ROOT","http://instatracks.dev/");
+
