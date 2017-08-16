1298f767862d81f4c838b8afbf70bc726da9387b
James Bunker <james@jamesbunker.com>
2017-08-16 14:42:37 +0100

    fix?

--- components/videotwo_page.php
+++ components/videotwo_page.php
@@ -9,20 +9,35 @@
 			require_once('lib/instagram/vendor/autoload.php');
 
 			// initialize class
-			$instagram = new Instagram(array(
+			$popular = new Instagram(array(
 			  'apiKey'      => INSTAGRAM_KEY,
 			  'apiSecret'   => INSTAGRAM_SECRET,
 			  'apiCallback' => WEB_ROOT.'instagram/popular'
 			));
 
+			$manual = new Instagram(array(
+			  'apiKey'      => INSTAGRAM_KEY,
+			  'apiSecret'   => INSTAGRAM_SECRET,
+			  'apiCallback' => WEB_ROOT.'instagram/manual'
+			));
+
+			$random = new Instagram(array(
+			  'apiKey'      => INSTAGRAM_KEY,
+			  'apiSecret'   => INSTAGRAM_SECRET,
+			  'apiCallback' => WEB_ROOT.'instagram'
+			));
+
 			// create login URL
-			$loginUrl = $instagram->getLoginUrl();
+
 
 			$this->tpl->setTemplate('videotwo');
 			$this->tpl->set('title','Instatracks Home');
 			$this->tpl->set('link1', '/videoone');
 			$this->tpl->set('link2', '/selectpics');
-			$this->tpl->set('popular_link', $loginUrl);
+			
+			$this->tpl->set('popular_link', $popular->getLoginUrl());
+			$this->tpl->set('manual_link', $manual->getLoginUrl());
+			$this->tpl->set('random_link', $random->getLoginUrl());
 		}
 
 	}
