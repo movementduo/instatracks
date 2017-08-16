ad73ec07fc0d2d7bcf0b98ccc72f5d70a2c36398
S435903 <jason@movement.co.uk>
2017-08-16 14:05:11 +0100

    popular for test

--- components/instagram_page.php
+++ components/instagram_page.php
@@ -31,8 +31,7 @@
 				// now you have access to all authenticated user methods
 				$result = $instagram->getUserMedia('self',100);
 				
-				// $mode = 'popular';
-				$mode = array_key_exists(1,$this->args)?$this->args[1]:'random';
+				$mode = array_key_exists(1,$this->args)?$this->args[1]:'popular';
 				
 				if(!in_array($mode,['random','popular','manual'])) {
 					$this->is404();
