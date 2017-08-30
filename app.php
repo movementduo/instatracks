8e693407e222ccb960b9daa51cc359b62a9ff2b5
S435903 <jason@movement.co.uk>
2017-08-30 16:59:34 +0100

    config clean up

--- app.php
+++ app.php
@@ -23,18 +23,11 @@ use Google\Cloud\Vision\VisionClient;
 $client = new Google_Client();
 $client->useApplicationDefaultCredentials();
 
-use Aws\Polly\PollyClient;
-$polly = new PollyClient([
-	'version' => 'latest',
-	'profile' => AWS_PROFILE,
-	'region' => AWS_REGION
-]);
-
 $i = new Instatracks;
 $i->setInstance($instanceID);
 $i->setLyrics(json_decode(file_get_contents('lyrics/'.APP_LANGUAGE.'.json')));
 $i->setVision(new VisionClient(['projectId' => GOOGLE_PROJECTID]));
-$i->setPolly($polly);
+$i->setPolly(new Aws\Polly\PollyClient(['version' => 'latest', 'profile' => AWS_PROFILE, 'region' => AWS_REGION]));
 $i->setS3(new Aws\S3\S3Client(['version' => 'latest', 'region' => S3_REGION]));
 $i->setDB($db);
 $i->execute();
